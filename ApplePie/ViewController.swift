5f971032004e4df7991d34c5cff311e0ad054787
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2023-12-03 17:03:13 +0300

    Interface created, added outlets and action

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lettersButtons: [UIButton]!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var treeImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
